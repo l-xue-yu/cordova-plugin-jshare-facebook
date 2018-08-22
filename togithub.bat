25ed8f0535997e5bdf7346d76cac338068663078
l-xue-yu <42016055+l-xue-yu@users.noreply.github.com>
2018-08-22 14:03:51 +0800

    Delete togithub.bat

--- togithub.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call echo "# cordova-plugin-jshare-facebook" >> README.md
-call git init
-call git add README.md
-call git add .
-call git commit -m "facebook·ÖÏí×Ó²å¼þ"
-call git remote add origin https://github.com/l-xue-yu/cordova-plugin-jshare-facebook.git
-call git pull origin master
-call git push -u origin master
